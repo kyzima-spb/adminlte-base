a87e850a86be8fb5363ac92d7820f4168cddd3aa
Kirill Vercetti <office@kyzima-spb.com>
2020-04-26 16:03:17 +0300

    Added constants

---
+++ adminlte_base/constants.py
@@ -0,0 +1,14 @@
+DEBUG = 'debug'
+ERROR = 'error'
+WARNING = 'warning'
+INFO = 'info'
+SUCCESS = 'success'
+
+
+ALERTS = {
+    DEBUG: ('debug', 'fas fa-bug'),
+    ERROR: ('danger', 'fas fa-ban'),
+    WARNING: ('warning', 'fas fa-exclamation-triangle'),
+    INFO: ('info', 'fas fa-info'),
+    SUCCESS: ('success', 'fas fa-check'),
+}
