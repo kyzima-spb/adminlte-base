9637caeec9eea203db53b7a921f8bd55c3f2db62
Kirill Vercetti <office@kyzima-spb.com>
2020-04-28 15:35:49 +0300

    Константы и базовые шаблоны

--- adminlte_base/constants.py
+++ adminlte_base/constants.py
@@ -1,14 +1,35 @@
+DEFAULT_SETTINGS = {
+    'ADMINLTE_SITE_TITLE': 'AdminLTE 3',
+
+    'ADMINLTE_BRAND_TEXT': 'AdminLTE 3',
+    'ADMINLTE_BRAND_IMAGE_ALT': 'AdminLTE Logo',
+
+    'ADMINLTE_ALLOW_REGISTRATION': True,
+    'ADMINLTE_REGISTRATION_ENDPOINT': 'auth.registration',
+    'ADMINLTE_LOGIN_ENDPOINT': 'auth.login',
+    'ADMINLTE_LOGOUT_ENDPOINT': 'auth.logout',
+
+    'ADMINLTE_PROFILE_ENDPOINT': 'user.profile',
+    'ADMINLTE_PASSWORD_RESET': True,
+    'ADMINLTE_PASSWORD_RESET_ENDPOINT': 'user.reset_password',
+}
+
+
+# Flash message levels
 DEBUG = 'debug'
 ERROR = 'error'
 WARNING = 'warning'
 INFO = 'info'
 SUCCESS = 'success'
+MESSAGE = 'message'
 
 
+# Flash message styles
 ALERTS = {
     DEBUG: ('debug', 'fas fa-bug'),
     ERROR: ('danger', 'fas fa-ban'),
     WARNING: ('warning', 'fas fa-exclamation-triangle'),
     INFO: ('info', 'fas fa-info'),
+    MESSAGE: ('info', 'fas fa-info'),
     SUCCESS: ('success', 'fas fa-check'),
 }
