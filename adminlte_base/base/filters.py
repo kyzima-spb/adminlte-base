18687b0246bcc513cb564c2b48bd7a92b0690768
Kirill Vercetti <office@kyzima-spb.com>
2020-06-05 14:45:51 +0300

    Добавлены все настройки из дочернего sidebar из демо

--- adminlte_base/base/filters.py
+++ adminlte_base/base/filters.py
@@ -2,6 +2,8 @@
 Provides ready-made implementations for filters used in templates.
 """
 
+from string import Template
+
 import arrow
 from dateutil import tz
 
@@ -16,19 +18,34 @@ def humanize(dt, locale='en_us', time_zone=None):
 
 def navbar_skin(color):
     """Returns a collection of classes to style the navigation bar."""
-    light = {ThemeColor.LIGHT, ThemeColor.WARNING, ThemeColor.WHITE, ThemeColor.ORANGE}
-    style = 'light' if color in light else f'dark'
-    return f'navbar-{style} navbar-{color}'
+    if color:
+        light = {ThemeColor.LIGHT, ThemeColor.WARNING, ThemeColor.WHITE, ThemeColor.ORANGE}
+        style = 'light' if color in light else f'dark'
+        return f'navbar-{style} navbar-{color}'
+    return ''
 
 
 def sidebar_skin(color, light=False):
     """"""
-    style = 'light' if light else f'dark'
-    return f'sidebar-{style}-{color}'
+    if color:
+        style = 'light' if light else f'dark'
+        return f'sidebar-{style}-{color}'
+    return ''
+
+
+def if_true(value, replace_with=None):
+    if not value:
+        return ''
+
+    if replace_with is None:
+        return value
+
+    return Template(replace_with).safe_substitute(value=value)
 
 
 __all__ = (
     humanize.__name__,
     navbar_skin.__name__,
-sidebar_skin.__name__,
+    sidebar_skin.__name__,
+    if_true.__name__,
 )
